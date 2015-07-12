acfe2f2b6db432fcb958778f34f3b7ffd3d7d457
David Obrite <david@ministrycentered.com>
2015-07-11 19:36:52 -0700

    notes

--- src/loc_map.rs
+++ src/loc_map.rs
@@ -8,4 +8,5 @@ use loc::Loc;
 
 pub type LocMap<T> = HashMap<Loc, Rc<RefCell<T>>>;
 
+// http://bluss.github.io/rust-itertools/doc/itertools/struct.RcIter.html
 // http://stackoverflow.com/questions/29345708/matching-a-generic-parameter-to-an-associated-type-in-an-impl
