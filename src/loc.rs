6e92ed898bc3e7d30ac536d35de343310c6fd5be
David Obrite <david@ministrycentered.com>
2015-07-17 11:28:17 -0700

    add and subtract locs

--- src/loc.rs
+++ src/loc.rs
@@ -1,5 +1,26 @@
+use std::ops::{
+    Add,
+    Sub,
+};
+
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 pub struct Loc {
     pub x: i32,
     pub y: i32,
+}
+
+impl Add for Loc {
+    type Output = Loc;
+
+    fn add(self, other: Loc) -> Loc {
+        Loc { x: self.x + other.x, y: self.y + other.y }
+    }
+}
+
+impl Sub for Loc {
+    type Output = Loc;
+
+    fn sub(self, other: Loc) -> Loc {
+        Loc { x: self.x - other.x, y: self.y - other.y }
+    }
 }
