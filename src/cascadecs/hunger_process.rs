e62fb133053dc0efe2d2b25395542d32e3293ab8
David Obrite <david@ministrycentered.com>
2015-08-23 17:45:34 -0700

    clean up hunger process

--- src/cascadecs/hunger_process.rs
+++ src/cascadecs/hunger_process.rs
@@ -1,21 +1,13 @@
 
-use piston::input::GenericEvent;
-
-use std::collections::HashMap;
-
 use cascadecs::event::Event;
-use cascadecs::entity::Entity;
 use cascadecs::process::Process;
 use cascadecs::components::Components;
-use cascadecs::hunger_component::HungerComponent;
 
-pub struct HungerProcess {
-    components: HashMap<Entity, HungerComponent>,
-}
+pub struct HungerProcess;
 
 impl HungerProcess {
-    pub fn new() -> HungerProcess {
-        HungerProcess { components: HashMap::new() }
+    pub fn new() -> Self {
+        HungerProcess
     }
 }
 
